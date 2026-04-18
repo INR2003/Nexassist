0797c243f2d342d6b7d5b0477e71b3d8b9fd8fd6
INR2003 <iyyanaranjalai8@gmail.com>
2026-04-18 21:57:31 +0530

    done

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -15,7 +15,7 @@ export default function LoginPage() {
       className="min-h-screen flex items-center justify-center bg-cover bg-center bg-no-repeat px-4"
         style={{
           backgroundImage: `url('/images/bg.png')`
-        }}
+        }} 
     >
       {/* Dark overlay */}
       <div className="absolute inset-0 bg-black/30" />
